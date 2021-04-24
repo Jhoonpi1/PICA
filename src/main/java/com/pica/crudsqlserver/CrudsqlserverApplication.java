575f0dc43fa73e391f7f89f0bbcfc7b23fa534e8
JHOOnpi <jhonalex196@gmail.com>
2021-04-24 16:14:23 -0500

    Version 1

---
+++ src/main/java/com/pica/crudsqlserver/CrudsqlserverApplication.java
@@ -0,0 +1,13 @@
+package com.pica.crudsqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudsqlserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudsqlserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pica/crudsqlserver/CrudsqlserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pica.crudsqlserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudsqlserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
